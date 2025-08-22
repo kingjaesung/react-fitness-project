2333025bbc8a5df9d82277b3ccec38a56a74b2ba
doolgi158 <kolonel@hanmail.net>
2025-08-22 10:30:29 +0900

    Header 날짜 수정 중

--- fitness/src/page/Home.js
+++ fitness/src/page/Home.js
@@ -1,24 +1,42 @@
 import Header from '../component/Header'
 import FitnessList from '../component/FitnessList'
 import Button from '../component/Button'
-import { useContext } from 'react'
+import { useContext, useState, useEffect } from 'react'
 import { FitnessStateContext } from '../App'
 import { useNavigate } from 'react-router-dom'
 
-const Home = ({date}) => {
+const Home = ({}) => {
     const data = useContext(FitnessStateContext);
     const navigate = useNavigate();
 
+    const [nowDate, setNowDate] = useState(new Date());
+    const title = `${nowDate.getFullYear()}년 ${nowDate.getMonth() + 1}월`;
+    const [filteredDate, setFilteredDate] = useState();
+    const beginTime = new Date(nowDate.getFullYear(), nowDate.getMonth(), 1, 1).getTime();
+    const endTime = new Date(nowDate.getFullYear(), nowDate.getMonth() + 1, 0, 23, 59, 59).getTime();
+
     const onClick = () => {
         navigate("/new");
-    }
+    };
+
+    useEffect(() => {
+        setFilteredDate(data.filter(it) => beginTime <= it.date && it.date <= endTime));
+    }, [data, nowDate]);
+
+    const increaseMonth = () => {
+        setNowDate(new Date(nowDate.getFullYear(), nowDate.getMonth() + 1));
+    };
+
+    const decreaseMonth = () => {
+        setNowDate(new Date(nowDate.getFullYear(), nowDate.getMonth() - 1));
+    };
 
     return (
         <div> 
             <Header
-                title={date}
-                left={<Button type="default" text="<"/>}
-                right={<Button type="default"text=">"/>}
+                title={title}
+                left={<Button type="default" text="<" onClick={decreaseMonth}/>}
+                right={<Button type="default"text=">" onClick={increaseMonth}/>}
             />
             <Button type="defalut" text="등록" onClick={onClick}/><hr />
             {data.map((it) =>(
