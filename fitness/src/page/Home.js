6c0e9900bb5eda9426de74df560daec7763eaac6
AnJaeSung <>
2025-08-22 14:48:41 +0900

    fitnessExam 1.35ver
    상세페이지 내부에서 새로고침시 에러표시 수정
    
    안재성

--- fitness/src/page/Home.js
+++ fitness/src/page/Home.js
@@ -1,52 +1,68 @@
-import Header from '../component/Header'
-import FitnessList from '../component/FitnessList'
-import Button from '../component/Button'
-import { useContext, useState, useEffect } from 'react'
-import { FitnessStateContext } from '../App'
-import { useNavigate } from 'react-router-dom'
+import Header from "../component/Header";
+import FitnessList from "../component/FitnessList";
+import Button from "../component/Button";
+import { useContext, useState, useEffect } from "react";
+import { FitnessStateContext } from "../App";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-    const data = useContext(FitnessStateContext);
-    const navigate = useNavigate();
+  const data = useContext(FitnessStateContext);
+  const navigate = useNavigate();
 
-    const [nowDate, setNowDate] = useState(new Date());
-    const title = `${nowDate.getFullYear()}년 ${nowDate.getMonth() + 1}월`;
-    const [filteredDate, setFilteredDate] = useState([]);
-       
-    const onClick = () => {
-        navigate("/new");
-    };
+  const [nowDate, setNowDate] = useState(new Date());
+  const title = `${nowDate.getFullYear()}년 ${nowDate.getMonth() + 1}월`;
+  const [filteredDate, setFilteredDate] = useState([]);
 
-    useEffect(() => {
-        const beginTime = new Date(nowDate.getFullYear(), nowDate.getMonth(), 1, 1).getTime();
-        const endTime = new Date(nowDate.getFullYear(), nowDate.getMonth() + 1, 0, 23, 59, 59).getTime();
-        
-        setFilteredDate(data.filter(it => { 
-            const targetTime = new Date(it.date).getTime();
-            return beginTime <= targetTime && targetTime <= endTime;
-        }));
-    }, [nowDate, data]);
+  const onClick = () => {
+    navigate("/new");
+  };
 
-    const increaseMonth = () => {
-        setNowDate(new Date(nowDate.getFullYear(), nowDate.getMonth() + 1));
-        console.log(filteredDate);
-    };
+  useEffect(() => {
+    const beginTime = new Date(
+      nowDate.getFullYear(),
+      nowDate.getMonth(),
+      1,
+      1
+    ).getTime();
+    const endTime = new Date(
+      nowDate.getFullYear(),
+      nowDate.getMonth() + 1,
+      0,
+      23,
+      59,
+      59
+    ).getTime();
 
-    const decreaseMonth = () => {
-        setNowDate(new Date(nowDate.getFullYear(), nowDate.getMonth() - 1));
-    };
+    setFilteredDate(
+      data.filter((it) => {
+        const targetTime = new Date(it.date).getTime();
+        return beginTime <= targetTime && targetTime <= endTime;
+      })
+    );
+  }, [nowDate, data]);
 
-    return (
-        <div> 
-            <Header
-                title={title}
-                left={<Button text="<" onClick={decreaseMonth}/>}
-                right={<Button text=">" onClick={increaseMonth}/>}
-            />
-            <Button text="등록" onClick={onClick}/><hr />
-            <FitnessList value={filteredDate}/>
-        </div>
-    )
-}
+  const increaseMonth = () => {
+    setNowDate(new Date(nowDate.getFullYear(), nowDate.getMonth() + 1));
+  };
+
+  const decreaseMonth = () => {
+    setNowDate(new Date(nowDate.getFullYear(), nowDate.getMonth() - 1));
+  };
+
+  return (
+    <div className="list_main">
+      <Header
+        title={title}
+        left={<Button text="<" onClick={decreaseMonth} />}
+        right={<Button text=">" onClick={increaseMonth} />}
+      />
+      <Button text="등록" onClick={onClick} />
+      <hr />
+      <div>
+        <FitnessList value={filteredDate} />
+      </div>
+    </div>
+  );
+};
 
 export default Home;
