afdf9a1c96d0464f5192569ef3bb05aa36e1a67d
Duda0826 <tjq0929@naver.com>
2025-08-23 15:07:16 +0900

    컨텐츠 누락부분 추가

--- fitness/src/page/Exercise.js
+++ fitness/src/page/Exercise.js
@@ -42,6 +42,7 @@ const Exercise = () => {
             count={target.count}
             date={target.date}
             calorie={target.calorie}
+            content={target.content}
         />
     </div>
   );
